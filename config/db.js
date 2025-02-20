a2f4ca58c7df634c7110354319b97be1b4599055
Kanugula Shivashanker <145932982+shivas1432@users.noreply.github.com>
2025-02-20 14:46:49 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,23 +1,27 @@
-import mysql2 from 'mysql2';
-import dotenv from 'dotenv';
+import mysql2 from "mysql2";
+import dotenv from "dotenv";
 
-// Load environment variables
 dotenv.config();
 
-const db = mysql2.createConnection({
-    host: process.env.DB_HOST,
+
+const db = mysql2.createPool({
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10, // Allow up to 10 connections
+  queueLimit: 0
 });
 
-// Connect to the database
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err.message);
-    } else {
-        console.log('Connected to the database.');
-    }
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed:", err.message);
+  } else {
+    console.log("Connected to the database.");
+    connection.release(); // Release the connection back to the pool
+  }
 });
 
 export default db;
